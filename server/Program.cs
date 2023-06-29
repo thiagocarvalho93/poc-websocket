f1df0a4ae323defb734ab418aa30933b2b2d00ce
thiagopaes.dsn.erp <thiagopaes.dsn.erp@alterdata.com.br>
2023-06-28 21:30:12 -0300

    🏗️ Organização dos projetos

--- server/Program.cs
+++ server/Program.cs
@@ -1,36 +1,32 @@
-using System.Net;
-using System.Net.WebSockets;
-using System.Text;
-
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
-app.UseWebSockets();
 
-
-app.Map("/", async context =>
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    if (!context.WebSockets.IsWebSocketRequest)
-        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-    else
-    {
-        using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
-        while (webSocket.State == WebSocketState.Open)
-        {
-            // checar novas autorizacoes pendentes
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-            // processar
+app.UseHttpsRedirection();
 
-            // mandar mensagens para os destinatários correspondentes
+var webSocketOptions = new WebSocketOptions
+{
+    KeepAliveInterval = TimeSpan.FromMinutes(2)
+};
 
-            // aguardar um tempo determinado
+app.UseWebSockets(webSocketOptions);
 
-            await webSocket.SendAsync(
-                Encoding.ASCII.GetBytes($"Alterdata -> {DateTime.Now}"),
-                WebSocketMessageType.Text,
-                true, CancellationToken.None);
-            await Task.Delay(5000);
-        }
-    }
-});
+app.UseAuthorization();
 
-await app.RunAsync();
+app.MapControllers();
+
+app.Run();
